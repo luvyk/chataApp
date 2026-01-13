5025f6afa5c33b9d18c1e998b1988ff4fb7cd650
vytislu2 <vytislu2@fit.cvut.cz>
2026-01-13 15:40:09 +0100

    Přidejte soubory projektu.

---
+++ Chat-ovaci-aplikace/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Chat_ovaci_aplikace.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat_ovaci_aplikace.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
