b09b3470a837c2c6e6dfcb123b95d83f0ef36ed9
Lukáš Vytisk <VytiskLukas@sssvt.cz>
2026-01-24 22:12:14 +0100

    jioji

---
+++ Chat-ovaci-aplikace/Controllers/ChataController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Chat_ovaci_aplikace.Controllers;
+using Eshop.Controllers;
+
+namespace Chat_ovaci_aplikace.Controllers
+{
+    public class ChataController : SecuredController
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
