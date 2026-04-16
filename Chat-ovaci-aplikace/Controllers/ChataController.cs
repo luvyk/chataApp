74cbf1d5c47040cf347f880c57c08565d152a362
luvyk <108091861+luvyk@users.noreply.github.com>
2026-04-16 02:52:22 +0200

    fixed some bugs

--- Chat-ovaci-aplikace/Controllers/ChataController.cs
+++ Chat-ovaci-aplikace/Controllers/ChataController.cs
@@ -18,31 +18,28 @@ namespace Chat_ovaci_aplikace.Controllers
             _databaseContext = context;
         }
 
+        [Route("chata/index/{idChata}")]
         public IActionResult Index([FromRoute] int idChaty)
         {
+            int idUzivatele = HttpContext.Session.GetInt32("Id") ?? 0;
+
             ViewBag.username = HttpContext.Session.GetString("User");
 
-            Chata chataDetail = _databaseContext.Chaty
-        .Where(c => c.IdChaty == idChaty + 1)
 
-    .Include(c => c.Mistnosti)
-        .ThenInclude(m => m.Mista)
+            List<Mistnost> mistnosti = _databaseContext.Mistnosti.Where(s => s.IdChaty == idChaty)
+                    .Include(s => s.Mista)
+                    .Include(s => s.Chata)
+                        .ThenInclude(s => s.Dny)
+                .ToList();
 
-    .Include(c => c.Dny)
-
-    .Include(c => c.Ucastnici)
-        .ThenInclude(u => u.Mista)
-            .ThenInclude(o => o.misto)
-
-    .Include(c => c.Ucastnici)
-        .ThenInclude(u => u.Mista)
-            .ThenInclude(o => o.den)
-
-    .FirstOrDefault();
-
+            Console.WriteLine("mistnosti načteny");
+            foreach(Mistnost m in mistnosti)
+            {
+                Console.WriteLine(m.IdChaty);
+            }
             
 
-            return View(chataDetail);
+            return View(mistnosti);
         }
 
         public IActionResult zapisNaMisto([FromQuery] int den,[FromQuery] int misto)
@@ -76,21 +73,26 @@ namespace Chat_ovaci_aplikace.Controllers
             return RedirectToAction("index");
         }
         [Route("chata/chataMainPage/{idChata}")]
-        public IActionResult chataMainPage(int idchata)
+        public IActionResult chataMainPage(int idChaty)
         {
-            Console.WriteLine($"idChaty je {idchata}");
+            int idUzivatele = HttpContext.Session.GetInt32("Id") ?? 0;
+            Ucastnik? ucastnik = _databaseContext.Ucastnici.FirstOrDefault(s => s.IdUzivatel == idUzivatele && s.IdChaty == idChaty);
+            ViewBag.idUcastnik = ucastnik.IdUcastnik;
+
+
             int idUcastnik = HttpContext.Session.GetInt32("Id") ?? 0;
-            List<Ukoly> ukoly = _databaseContext.Ukoly.Where(s => s.Den.IdChaty == idchata && s.IdUcastnik == idUcastnik).ToList();
+            List<Ukoly> ukoly = _databaseContext.Ukoly.Where(s => s.Den.IdChaty == idChaty && s.IdUcastnik == idUcastnik).ToList();
 
             List<ProgramChaty> programy = _databaseContext.Programy.Where(s => _databaseContext.Dny
-                                        .Where(s => s.IdChaty == idchata)
+                                        .Where(s => s.IdChaty == idChaty)
                                         .Select(s => s.IdDen).Contains(s.IdDen)).ToList();
-            return View(idchata);
+            return View(idChaty);
         }
         [Route("chata/AkceAUkoly/{idChata}")]
-        public IActionResult AkceAUkoly(int idChata)
+        public IActionResult AkceAUkoly(int idChata, [FromQuery] int idUcastnik)
         {
-            int idUcastnik = HttpContext.Session.GetInt32("Id") ?? 0;
+            //int idUzivatel = HttpContext.Session.GetInt32("Id") ?? 0;
+            ViewBag.Id = idUcastnik;
 
             List<Akce> akce = _databaseContext.Akces.Where(s => s.Den.IdChaty == idChata).ToList();
             List<Ukoly> ukoly = _databaseContext.Ukoly
@@ -98,6 +100,14 @@ namespace Chat_ovaci_aplikace.Controllers
 
             AkceAUkolyViewModel model = new AkceAUkolyViewModel(akce, ukoly);
             return View(model);
+        }
+
+        [Route("chata/PrihlasitSeNaAkci/{idChata}")]
+        public IActionResult PrihlasitSeNaAkci([FromQuery] int idAkce, [FromRoute]int idChata)
+        {
+
+
+            return RedirectToAction("chataMainPage", new { idChata = idChata});
         }
     }
 }
